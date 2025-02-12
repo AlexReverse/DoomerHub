92bcb60fd2179a20bcde6533a7ce4446fc915739
AlexReverse <mr.evteev.1998@gmail.com>
2025-02-12 20:33:06 +0300

    12.02: добавлено отображение ошибок при заполнении постов

--- src/main/java/com/hub/doomer/controller/PostController.java
+++ src/main/java/com/hub/doomer/controller/PostController.java
@@ -1,22 +1,36 @@
 package com.hub.doomer.controller;
 
 import com.hub.doomer.controller.payload.UpdatePostPayload;
+
 import com.hub.doomer.entity.Post;
 import com.hub.doomer.service.PostService;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
+import org.springframework.context.MessageSource;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Locale;
+import java.util.NoSuchElementException;
+
 @Controller
-@RequestMapping("search/posts/{postId:\\d+}")
+@RequestMapping(("search/posts/{postId:\\d+}"))
 @RequiredArgsConstructor
 public class PostController {
 
     private final PostService postService;
+
+    private final MessageSource messageSource;
     @ModelAttribute("post")
     public Post post(@PathVariable("postId") int postId){
-        return this.postService.findPost(postId).orElseThrow();
+        return this.postService.findPost(postId)
+                .orElseThrow(() -> new NoSuchElementException("search.errors.post.not_found"));
     }
     @GetMapping
     public String getPost() {
@@ -29,14 +43,33 @@ public class PostController {
     }
 
     @PostMapping("edit")
-    public String updatePost(@ModelAttribute("post") Post post, UpdatePostPayload updatePostPayload) {
-        this.postService.updatePost(post.getId(), updatePostPayload.title(), updatePostPayload.description());
-        return "redirect:/search/posts/%d".formatted(post.getId());
+    public String updatePost(@ModelAttribute(name = "post", binding = false) Post post,
+                             @Valid UpdatePostPayload updatePostPayload, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            model.addAttribute("payload", updatePostPayload);
+            model.addAttribute("errors", result.getAllErrors().stream()
+                    .map(ObjectError::getDefaultMessage)
+                    .toList());
+            return "search/posts/edit";
+        } else {
+            this.postService.updatePost(post.getId(), updatePostPayload.title(), updatePostPayload.description());
+            return "redirect:/search/posts/%d".formatted(post.getId());
+        }
     }
 
     @PostMapping("delete")
     public String deletePost(@ModelAttribute("post") Post post) {
         this.postService.deletePost(post.getId());
         return "redirect:/search/posts/list";
+    }
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public String handleNoSuchElementException(NoSuchElementException exception, Model model,
+                                               HttpServletResponse response, Locale locale) {
+        response.setStatus(HttpStatus.NOT_FOUND.value());
+        model.addAttribute("error",
+                this.messageSource.getMessage(exception.getMessage(), new Object[0],
+                        exception.getMessage(), locale));
+        return "search/errors/404";
     }
 }
--- src/main/java/com/hub/doomer/controller/payload/UpdatePostPayload.java
+++ src/main/java/com/hub/doomer/controller/payload/UpdatePostPayload.java
@@ -1,4 +1,11 @@
 package com.hub.doomer.controller.payload;
 
-public record UpdatePostPayload(String title, String description) {
-}
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public record UpdatePostPayload(
+        @NotNull(message = "{search.posts.update.errors.title_is_null}")
+        @Size(min = 3, max = 50, message = "{search.posts.update.errors.title_size_is_invalid}")
+        String title,
+        @Size(min = 100, max = 5000, message = "{search.posts.update.errors.description_size_is_invalid}")
+        String description) {}
