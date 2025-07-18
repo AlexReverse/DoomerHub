5ddad85d5c648c26d7040a4b37f05ea54615da8c
AlexReverse <mr.evteev.1998@gmail.com>
2025-07-18 21:16:20 +0300

    18.07: добавил БД для работы с информацией пользователя

---
+++ info-service/src/main/java/org/alexreverse/entity/MainPage.java
@@ -0,0 +1,59 @@
+package org.alexreverse.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.relational.core.mapping.Table;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(schema = "infoservice", name = "main_page")
+public class MainPage {
+
+    @Id
+    @Column(name = "user_id")
+    private UUID userId;
+
+    @Column(name = "nickname")
+    @NotNull
+    @Size(min = 3, max = 50)
+    private String nickname;
+
+    @Column(name = "name")
+    @NotNull
+    @Size(min = 3, max = 50)
+    private String name;
+
+    @Column(name = "sur_name")
+    @NotNull
+    @Size(min = 3, max = 50)
+    private String surName;
+
+    @Column(name = "city")
+    @NotNull
+    @Size(min = 3, max = 50)
+    private String city;
+
+    @Column(name = "age")
+    @NotNull
+    @Size(min = 14, max = 100)
+    private Byte age;
+
+    @Column(name = "description")
+    @Size(max = 100)
+    private String description;
+
+    @Column(name = "post_date")
+    @NotNull
+    private LocalDateTime postDate;
+}
